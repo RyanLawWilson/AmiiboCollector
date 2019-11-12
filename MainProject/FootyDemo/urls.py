a586619f2edcd2e8eb9d58af36ec03c694acfcd0
deanna.cochener <deanna.cochener@gmail.com>
2019-11-12 12:14:44 -0800

    simple full CRUD

--- MainProject/FootyDemo/urls.py
+++ MainProject/FootyDemo/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.index, name='footy'),
     path('/AddToCollection/', views.add_jersey, name='createJersey'),
     path('<int:pk>/Details/', views.details_jersey, name='jerseyDetails'),
+    path('<int:pk>/Edit/', views.edit_jersey, name='jerseyEdit'),
+    path('<int:pk>/Delete/', views.delete_jersey, name='jerseyDelete'),
     ]
--- MainProject/FootyDemo/views.py
+++ MainProject/FootyDemo/views.py
@@ -21,8 +21,34 @@ def add_jersey(request):
         form = JerseyForm()
     return render(request, 'FootyDemo/footy_create.html', {'form':form})
 
+
 def details_jersey(request, pk):
     pk = int(pk)
     jersey = get_object_or_404(Jersey, pk=pk)
     context={'jersey':jersey}
     return render(request,'FootyDemo/footy_details.html', context)
+
+
+def edit_jersey(request, pk):
+    pk = int(pk)
+    jersey = Jersey.Jerseys.get(pk=pk)
+    form = JerseyForm(data=request.POST or None, instance=jersey)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('jerseyDetails',pk)
+        else:
+            print(form.errors)
+    else:
+        return render(request,'FootyDemo/footy_edit.html', {'form':form})
+
+
+def delete_jersey(request, pk):
+    pk = int(pk)
+    jersey = Jersey.Jerseys.get(pk=pk)
+    context = {'jersey': jersey}
+    if request.method == 'POST':
+        jersey.delete()
+        return redirect('footy')
+    else:
+        return render(request, 'FootyDemo/footy_delete.html', context)
