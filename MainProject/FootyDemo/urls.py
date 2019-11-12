94dc2bf1155cdbc3c9bfb4d4a6406a670c14b682
deanna.cochener <deanna.cochener@gmail.com>
2019-11-12 12:59:53 -0800

    full CRUD tested

--- MainProject/FootyDemo/urls.py
+++ MainProject/FootyDemo/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='footy'),
-    path('/AddToCollection/', views.add_jersey, name='createJersey'),
+    path('AddToCollection/', views.add_jersey, name='createJersey'),
     path('<int:pk>/Details/', views.details_jersey, name='jerseyDetails'),
     path('<int:pk>/Edit/', views.edit_jersey, name='jerseyEdit'),
     path('<int:pk>/Delete/', views.delete_jersey, name='jerseyDelete'),
