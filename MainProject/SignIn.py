e2463aa6f7609f32500a9ce5854929c95f1361e7
deanna.cochener <deanna.cochener@gmail.com>
2019-11-22 16:58:54 -0800

    sign in

--- MainProject/SignIn.py
+++ MainProject/SignIn.py
@@ -1,3 +1,6 @@
 # Add your name on a line below
 
-# Deanna Cochener
+# > Deanna Cochener
+#     > Soccer jersey collection app
+#     >API: api.football-data.org
+#     >Data scrape: https://www.mlssoccer.com/news
