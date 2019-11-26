251a83ee96c1295ed92b9a83e71797958ee6c21b
Clint Hughes <cahughes27@gmail.com>
2019-11-25 17:44:11 -0800

    Add my Changes

--- MainProject/SignIn.py
+++ MainProject/SignIn.py
@@ -1,6 +1,6 @@
 # Add your name on a line below
 
-# > Deanna M Cochener
-#     > Soccer jersey collection app
-#     >API: api.football-data.org
-#     >Data scrape: https://www.mlssoccer.com/news
+# > Clint Hughes
+#     > Game of Thrones Collection App
+#     >API: https://anapioficeandfire.com/
+#     >Data scrape: https://www.hbo.com/game-of-thrones
