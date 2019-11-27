31bdd01f69dfdae9e41b40f751ab976d362080aa
deanna.cochener <deanna.cochener@gmail.com>
2019-11-26 18:47:30 -0800

    Resetting to Basic Project for clean slate

--- MainProject/SignIn.py
+++ MainProject/SignIn.py
@@ -4,18 +4,3 @@
 #     > Soccer jersey collection app
 #     >API: api.football-data.org
 #     >Data scrape: https://www.mlssoccer.com/news
-
-#> Connor Sharpe
-#   > Healthy Recipe Collection
-#   > API: edamam.com/edamam-recipe-api
-#   > Data scrape: https://www.sciencedaily.com/news/health_medicine/nutrition/
-
-# > Clint Hughes
-#     > Game of Thrones Collection App
-#     >API: https://anapioficeandfire.com/
-#     >Data scrape: https://www.hbo.com/game-of-thrones
-
-# > Kyla Kozole
-#   > Contemporary Art Collection App
-#   > API: https://developers.artsy.net/v2
-#   > Data scrape: http://www.contemporaryartdaily.com/
