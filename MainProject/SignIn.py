4fe99191a931194da93213e0502a2b7f4061af7e
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-02-10 16:13:17 -0700

    Sign In | Plan for Project

--- MainProject/SignIn.py
+++ MainProject/SignIn.py
@@ -7,3 +7,23 @@
 #     > Soccer jersey collection app
 #     >API: api.football-data.org
 #     >Data scrape: https://www.mlssoccer.com/news
+
+# I broke everything down so that I could figure out what I wanted to do.  I included some
+# features that I think would be cool to implement as well.  We'll see if I get to them.
+#
+# * - MVP
+# ** - A nice added feature
+# *** - A difficult to add feature
+
+# > Ryan L Wilson
+#	> Amiibo collection app
+#		* Allow user to add amiibos
+#		*** User can select listed amiibos on the API page and mass add them to the database.
+#	> API: https://www.amiiboapi.com/
+#		* Show the 20 most recent amiibos
+#		* Allow user options to sort through all amiibos using API
+#		** Show the images of each amiibo
+#		*** When the user clicks on the image, they are redirected to a "more information" page
+#	> Data scrape: https://www.nintendo.com/whatsnew/#all
+#		* Show latest Nintendo news.
+#		*** Show any news related to amiibos first
