fdbaeb3c15c62900732de638776cb7e4de61e9d3
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-02-11 08:25:48 -0800

    Completed sign in

--- MainProject/SignIn.py
+++ MainProject/SignIn.py
@@ -7,3 +7,8 @@
 #     > Soccer jersey collection app
 #     >API: api.football-data.org
 #     >Data scrape: https://www.mlssoccer.com/news
+
+# > Hanna Wilson
+#       > Book collection app
+#       > API: https://openlibrary.org/developers/api
+#       > Data scrape: https://www.barnesandnoble.com/b/books/_/N-29Z8q8
