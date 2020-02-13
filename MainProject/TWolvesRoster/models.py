d951601d227c668c4a76a95a07db8514029717dd
Tyler Lyons <tlyons030@gmail.com>
2020-02-13 15:35:59 -0700

    Story2: Crteated model, form, template, and view. Getting a migration error.

--- MainProject/TWolvesRoster/models.py
+++ MainProject/TWolvesRoster/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+POSITION_OPTIONS = (('Point Guard', 'Point Guard'), ('Shooting Guard', 'Shooting Guard'), ('Small Forward', 'Small Forward'),
+                    ('Power Forward', 'Power Forward'), ('Center', 'Center'))
+
+STATUS_OPTIONS = (('Current Player', 'Current Player'), ('Retired', 'Retired'), ('Plays for another team', 'Plays for another team'))
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=60, null=False, blank=False)
+    position = models.CharField(max_length=75, Choices=POSITION_OPTIONS)
+    currentStatus = models.CharField(max_length=75, Choices=STATUS_OPTIONS)
+    age = models.PositiveIntegerField(blank=True, null=True)
+
+    Players = models.Manager()
+
+    def __str__(self):
+        return self.name
--- MainProject/TWolvesRoster/views.py
+++ MainProject/TWolvesRoster/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from.models import Player
+from.forms import PlayerForm
+from django.shortcuts import render, redirect, get_object_or_404
 
 
 # Create your views here.
@@ -8,7 +11,14 @@ def wolves_home(request):
 
 
 def wolves_create(request):
-    return render(request, "TWolvesRoster/wolves_create.html")
+    form = PlayerForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('viewPlayers')
+    else:
+        print(form.errors)
+        form = PlayerForm()
+    return render(request, "TWolvesRoster/wolves_create.html", {'form':form})
 
 
 def wolves_players(request):
