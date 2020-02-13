e29c687191ce5b3bb3750943a7632f3092edef66
Brianna <Brianna@BriannaWin10PC.com>
2020-02-13 11:22:53 -0800

    sign in

--- MainProject/SignIn.py
+++ MainProject/SignIn.py
@@ -12,3 +12,9 @@
 #     > Places to surf App
 #     >API: https://tidesandcurrents.noaa.gov/api/
 #     >Data Scrape: https://www.surfline.com/surf-news/
+
+
+# > Brianna Keany
+#   > Community/college radio collection
+#   > API: https://api.radio-browser.info/ || https://www.programmableweb.com/api/radio-browser-rest-api
+#   > Data Scrape: https://xray.fm/playlist
