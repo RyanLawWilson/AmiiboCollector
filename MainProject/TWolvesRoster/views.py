3a2115e83c75af9f7fe261736e5a27e11704fc40
Tyler Lyons <tlyons030@gmail.com>
2020-02-12 17:11:01 -0700

    Story2: fixes my styling and added more templates with button links

--- MainProject/TWolvesRoster/views.py
+++ MainProject/TWolvesRoster/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
+def wolves_home(request):
     return render(request, "TWolvesRoster/wolves_home.html")
+
+
+def wolves_create(request):
+    return render(request, "TWolvesRoster/wolves_create.html")
+
+
+def wolves_players(request):
+    return render(request, "TWolvesRoster/wolves_players.html")
