bce330471ab7f415e008429afae7c892b743bd19
T33mac <31869841+T33mac@users.noreply.github.com>
2020-02-15 19:04:07 -0800

    add database and html files

--- MainProject/WaveWatch/urls.py
+++ MainProject/WaveWatch/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.WaveHome, name='waves'),
+    path('Collection/', views.index, name='listSurfType'),  # index of jerseys
+    path('AddTo/', views.add_surfInfo, name='addSurfInfo'),  # index of jerseys
+    #path('ApiService/', views.api_response, name='wavesApi'),  # main page for API service with dropdowns
 ]
