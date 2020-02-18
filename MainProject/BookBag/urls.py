230ff2e6ae8d06445d32c98072d0f14d65bfad46
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-02-18 14:38:06 -0800

    Completed Story #4
    Created a details page, linked it to each book in the library, and created function to render all the details for whichever book is selected.
    
    Edited the styling a little more on the add to library page

--- MainProject/BookBag/urls.py
+++ MainProject/BookBag/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.bookBagHome, name='bookBagHome'),                #home page
-    path('AddToLibrary/', views.add_book, name='createBook'),
-    path('Library/', views.index, name='listBooks'),
+    path('', views.bookBagHome, name='bookBagHome'),                # HOME PAGE
+    path('AddToLibrary/', views.add_book, name='createBook'),       # ADD BOOK TO LIBRARY
+    path('Library/', views.index, name='listBooks'),                # VIEW LIBRARY
+    path('Library/<int:pk>/Details/', views.details_book, name='bookDetails')             # VIEW SINGLE BOOK DETAIL
 
     ]
