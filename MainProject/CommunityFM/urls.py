0a8786bf0342ef62e4e9f5cd43d78ca27fa34883
Brianna <Brianna@BriannaWin10PC.com>
2020-02-18 13:25:46 -0800

    create index and update styles

--- MainProject/CommunityFM/urls.py
+++ MainProject/CommunityFM/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.radioHome, name='radioHome'),
-    path('addNewStation/', views.add_station, name='addStation')
+    path('addNewStation/', views.add_station, name='addStation'),
+    path('index/', views.station_index, name='index')
     ]
