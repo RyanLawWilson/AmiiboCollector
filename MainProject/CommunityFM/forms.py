6a23e05a5c514acd1febfbca0087092c32e26477
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-03-19 18:13:51 -0600

    Delete other people's projects

--- MainProject/CommunityFM/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Station
-
-#create the form class
-class StationForm(ModelForm):
-    class Meta:
-        model = Station
-        fields = '__all__'
